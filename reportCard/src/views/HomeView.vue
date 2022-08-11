ed65fd3c4ff42a6e6fb0a2a705eba176eac2e37b
Inma Mimateach <inmagdiaz92@gmail.com>
2022-08-11 17:46:20 +0200

    testeo de funcionamiento de la app

---
+++ reportCard/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MarksView from "@/views/MarksView.vue";
+</script>
+
+<template>
+  <main>
+    <button>
+      <RouterLink to="./views/MarksView.vue">Go to mark's reports</RouterLink>
+    </button>
+    
+  </main>
+</template>
---
+++ reportCard/src/views/MarksView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Header from "../components/Header.vue";
+</script>
+
+<template>
+  <main>
+    <Header />
+    <h3>
+      You’ve successfully created a project with
+    </h3>
+        <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+    </h3>
+
+  </main>
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+h3 {
+    color: blue;
+}
+</style>
